fccdbfb6f07063a4097ac2a8b03b964a1b62e10c
Mohammed Ismail Ansari <team.terminal@gmail.com>
2018-12-06 19:22:18 +0000

    add template files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # lay-em
-A library to create cross-browser, self-adjusting HTML layouts [coming-soon]
+
+[![npm version](https://badge.fury.io/js/lay-em.svg)](https://badge.fury.io/js/lay-em)
+[![npm downloads](https://img.shields.io/npm/dt/lay-em.svg)](https://www.npmjs.com/package/lay-em)  
+[![Build Status](https://travis-ci.org/myTerminal/lay-em.svg?branch=master)](https://travis-ci.org/myTerminal/lay-em)
+[![Code Climate](https://codeclimate.com/github/myTerminal/lay-em.png)](https://codeclimate.com/github/myTerminal/lay-em)
+[![Coverage Status](https://img.shields.io/coveralls/myTerminal/lay-em.svg)](https://coveralls.io/r/myTerminal/lay-em?branch=master)  
+[![Dependency Status](https://david-dm.org/myTerminal/lay-em.svg)](https://david-dm.org/myTerminal/lay-em)
+[![devDependency Status](https://david-dm.org/myTerminal/lay-em/dev-status.svg)](https://david-dm.org/myTerminal/lay-em#info=devDependencies)
+[![peer Dependency Status](https://david-dm.org/myTerminal/lay-em/peer-status.svg)](https://david-dm.org/myTerminal/lay-em#info=peerDependencies)  
+[![License](https://img.shields.io/github/license/myTerminal/lay-em.svg)](https://opensource.org/licenses/MIT)  
+[![NPM](https://nodei.co/npm/lay-em.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/lay-em/)
+
+A library to create cross-browser, self-adjusting HTML layouts [coming soon]
---
+++ webpack.common.js
@@ -0,0 +1,85 @@
+/* global require module __dirname */
+
+const libraryFileName = 'lay-em';
+const libraryName = 'LayEm';
+
+const sourceDir = 'src';
+const outputDir = 'build';
+
+const path = require('path');
+
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const clean = new CleanWebpackPlugin([outputDir]);
+
+const extractCSS = new ExtractTextPlugin('styles/' + libraryFileName + '.css');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        library: './' + sourceDir + '/scripts/' + libraryFileName + '.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'fonts/[name].[ext]',
+                            publicPath: '../'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(less|css)$/,
+                use: extractCSS.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                minimize: true
+                            }
+                        },
+                        {
+                            loader: 'less-loader'
+                        }
+                    ]
+                })
+            },
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'eslint-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        clean,
+        extractCSS
+    ],
+    output: {
+        filename: 'scripts/' + libraryFileName + '.js',
+        path: path.resolve(__dirname, outputDir),
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    }
+};
