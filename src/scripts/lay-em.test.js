6a3880f0d545f889feff18642bf50226ae9ea4d5
myTerminal <team.terminal@gmail.com>
2018-12-15 00:31:29 -0600

    add infrastructure for unit tests

---
+++ src/scripts/lay-em.test.js
@@ -0,0 +1,9 @@
+/* global describe it */
+
+import layEm from './lay-em.js';
+
+describe('Basic tests', function () {
+    it('Knows that true it true', function () {
+        true.should.equal(true);
+    });
+});
