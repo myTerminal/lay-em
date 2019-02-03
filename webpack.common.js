dbaac4c3a545c365b9e68d31490cbf998d50a07c
myTerminal <team.terminal@gmail.com>
2019-02-03 12:23:31 -0600

    remove unrequired dependencies and configuration

--- webpack.common.js
+++ webpack.common.js
@@ -10,12 +10,8 @@ const path = require('path');
 
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const clean = new CleanWebpackPlugin([outputDir]);
-const extractCSS = new ExtractTextPlugin('styles/' + libraryFileName + '.css');
-const optimizeCSS = new OptimizeCssAssetsPlugin();
 
 module.exports = {
     mode: 'development',
@@ -24,30 +20,6 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: 'fonts/[name].[ext]',
-                        publicPath: '../'
-                    }
-                }
-            },
-            {
-                test: /\.(less|css)$/,
-                use: extractCSS.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader'
-                        },
-                        {
-                            loader: 'less-loader'
-                        }
-                    ]
-                })
-            },
             {
                 test: /\.js$/,
                 enforce: 'pre',
@@ -62,9 +34,7 @@ module.exports = {
         ]
     },
     plugins: [
-        clean,
-        extractCSS,
-        optimizeCSS
+        clean
     ],
     output: {
         filename: 'scripts/[name].js',
